b55cadf5d6aa22289ae9844fa3f1732d6a9e5825
Abdulrahman <ahmed.abdulrahman@capgemini.com>
2021-06-16 09:16:39 +0200

    speaker model done with annotations, getters and setters

--- src/main/java/com/jack/huncho/conference/model/Session.java
+++ src/main/java/com/jack/huncho/conference/model/Session.java
@@ -1,12 +1,23 @@
 package com.jack.huncho.conference.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.time.LocalTime;
 import java.util.List;
 
+@Entity
 public class Session {
+
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private LocalTime start;
     private LocalTime end;
+
+    @ManyToMany()
+    @JsonIgnore
     private List<Speaker> speakers;
     private int length;
     private String description;
--- src/main/java/com/jack/huncho/conference/model/Speaker.java
+++ src/main/java/com/jack/huncho/conference/model/Speaker.java
@@ -1,12 +1,93 @@
 package com.jack.huncho.conference.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity(name = "speakers")
 public class Speaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @ElementCollection
     private List<String> skills;
-    private Session sessions;
+
+    @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
+    private List<Session> sessions;
+
     private String title;
     private String company;
+    private String bio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
